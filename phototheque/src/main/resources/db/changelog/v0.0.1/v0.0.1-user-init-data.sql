39910fcfdefd17594ac098edeb482961b384fe65
lcqamau <104760365+lcqamau@users.noreply.github.com>
2024-02-21 12:28:46 +0100

    first commit

--- phototheque/src/main/resources/db/changelog/v0.0.1/v0.0.1-user-init-data.sql
+++ phototheque/src/main/resources/db/changelog/v0.0.1/v0.0.1-user-init-data.sql
@@ -1,2 +1,6 @@
+insert into users (firstname,lastname) values ('Anakin','Skywalker');
+insert into users (firstname,lastname) values ('Luke','Skywalker');
+insert into users (firstname,lastname) values ('AObi-Wan','Kenobi');
+insert into users (firstname,lastname) values ('Leia','Organa');
 
 
